33a3d2429a53ddf9d733216c08855d9e4f4747f3
Kulani <kulimanganye@gmail.com>
2023-07-21 15:10:40 +0200

    initial code

---
+++ band_name_generator.py
@@ -0,0 +1,9 @@
+# the band name generator will work by taking in the name of the city you grew up in and your pet name then constructing a band name from those names
+
+print("Hello! Welcome to band name generator")
+
+city = input("What is the name of the city you grew up in? \n ")
+
+pet = input("What is the name of your pet? \n ")
+
+print("name of band is: " + city + " " + pet)
